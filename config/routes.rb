f68c6724fa9c205dae97db37f590052529c23303
Tai Ly <camtai741@gmail.com>
2017-09-20 00:26:29 +0700

    Create Menu page and Contact Us page. Add address and phone number to Contact Us page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'menu#index'
+
+  resources :menu , only: [:index]
+  resources :contact_us , only: [:index]
+
 end

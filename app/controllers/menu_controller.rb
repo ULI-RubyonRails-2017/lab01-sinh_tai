cb9101164fa346c617bc2937aeca83ab42359867
Tai Ly <camtai741@gmail.com>
2017-09-23 09:12:54 +0700

    Implement a basic menu

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,4 +1,15 @@
 class MenuController < ApplicationController
   def index
+    @menu_items_breakfast = MenuItem.where(section: 'breakfast')
+    @menu_items_breakfast = [] if @menu_items_breakfast == nil
+
+    @menu_items_lunch = MenuItem.where(section: 'lunch')
+    @menu_items_breakfast = [] if @menu_items_breakfast == nil
+
+    @menu_items_dinner = MenuItem.where(section: 'dinner')
+    @menu_items_breakfast = [] if @menu_items_breakfast == nil
+
+    @menu_items_drinks = MenuItem.where(section: 'drinks')
+    @menu_items_breakfast = [] if @menu_items_breakfast == nil
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170922142526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "menu_items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.string "section"
+    t.text "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
